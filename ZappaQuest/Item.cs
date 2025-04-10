242c2c6745699d8381a0f79e4ce7bcae6b52b707
Jade Bell <bellj22@nku.edu>
2025-04-09 20:43:58 -0400

    First changes

---
+++ ZappaQuest/Item.cs
@@ -0,0 +1,11 @@
+using System;
+namespace ZappaQuest
+
+
+public class Item {
+    // Type and Description are String
+    public string Type {get; set;}
+    public string Description {get; set;}
+    // Have a boolean to check if item is magical or not
+    public boolean isMagical {get; set;}
+}
